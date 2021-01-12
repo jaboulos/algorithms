766011ce2bf081b040d7d228ecfbec13b88ae074
jaboulos <jab12288@gmail.com>
2021-01-12 15:57:48 -0800

    Step through each stack frame

--- src/recursion/dialPad/dialPad.js
+++ src/recursion/dialPad/dialPad.js
@@ -1,33 +1,7 @@
-/*
-leetcode link: https://leetcode.com/problems/letter-combinations-of-a-phone-number/
-given input phone number,
-looking at a dialpad, each digit has letters
-permute each character associated with digit on dialpad
-
-1 2 3
-4 5 6
-7 8 9
-  0
-
-{
-  1: '1',
-  2: 'abc',
-  3: 'def',
-  4: 'ghi',
-  5: 'jkl',
-  6: 'mno',
-  7: 'pqrs',
-  8: 'tuv',
-  9: 'wxyz',
-  0: '0'
-}
-
-*/
-
 const dialPad = (digits) => {
   // create obj with mapped values for each digit
   let charMap = {
-    // 1: "1",
+    1: "1",
     2: "abc",
     3: "def",
     4: "ghi",
@@ -36,26 +10,41 @@ const dialPad = (digits) => {
     7: "pqrs",
     8: "tuv",
     9: "wxyz",
-    // 0: "0",
+    0: "+",
   };
 
   let result = helper(charMap, digits, [], "", digits.length);
   return result;
 };
-
-const helper = (charMap, digit, result = [], currentStr = "", len) => {
+//                        '1230'  []        ''        4
+//                        '230'   []        '1'       4
+//                         '30'   []        '1a'      4
+//                          '0'   []        '1ad'     4
+//                          ''    []        '1ad+'    4
+const helper = (charMap, digits, result, currentStr, len) => {
+  // hit BC
   if (currentStr.length === len) {
+    // result.push('1ad+')
     result.push(currentStr);
-    return [];
   } else {
-    for (let i = 0; i < digit.length; i++) {
-      let char = digit[i];
-      let str = charMap[char];
-      let nextDigit = digit.slice(i + 1);
+    //                      3
+    //                      2
+    //                      1
+    for (let i = 0; i < digits.length; i++) {
+      let char = digits[i]; // 1 // 2 // 3 // 0
+      let str = charMap[char]; // '1' // 'abc' // 'def' // '+'
+      let nextDigits = digits.slice(i + 1); // '230' // '30' // '0' // ''
       for (let j = 0; j < str.length; j++) {
-        helper(charMap, nextDigit, result, currentStr + str[j], len);
+        //                '230'      []         '' + '1'          4
+        //                '30'       []         '1' + 'a'         4
+        //                '0'        []         '1a' + 'd'        4
+        //                 ''        []         '1ad' + '+'       4
+        helper(charMap, nextDigits, result, currentStr + str[j], len);
       }
     }
   }
+  // goes to previous stack in recursive calls, take recursive leap of faith
   return result;
 };
+
+console.log(dialPad("1230"));
