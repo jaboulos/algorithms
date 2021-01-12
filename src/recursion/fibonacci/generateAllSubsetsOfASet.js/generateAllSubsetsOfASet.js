609c188ce3703eb777fc3917e242ea1282209de4
jaboulos <jab12288@gmail.com>
2021-01-11 18:28:28 -0800

    Add generateAllSubsetsOfASet problem

---
+++ src/recursion/fibonacci/generateAllSubsetsOfASet.js/generateAllSubsetsOfASet.js
@@ -0,0 +1,22 @@
+/*
+Generate All Subsets Of A Set
+Generate ALL possible subsets of a given set. The set is given in the form of a string s containing distinct lowercase characters 'a' - 'z'.
+
+Example
+Input: "xy"
+Output: ["", "x", "y", "xy"]
+
+Notes
+Input Parameters: There is only one argument denoting string s.
+Output: Return array of strings res, containing ALL possible subsets of given string. You need not to worry about order of strings in your output array. E.g. s = "a", arrays ["", "a"] and ["a", ""]  both will be accepted.
+
+Order of characters in any subset must be same as in the input string. For s = "xy", array ["", "x", "y", "xy"] will be accepted, but ["", "x", "y", "yx"] will not be accepted.
+
+Constraints:
+0 <= |s| <= 20
+s only contains distinct lowercase alphabetical letters ('a' - 'z').
+
+Empty set is a subset of any set.
+
+Any set is a subset of itself.
+*/
