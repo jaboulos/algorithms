6a316e15043e981800147455f6f702d6729980ef
jaboulos <jab12288@gmail.com>
2021-01-12 15:58:06 -0800

    Initial setup for permutations algorithm

---
+++ src/recursion/permutationsOfAGivenString/permutationsOfAGivenString.js
@@ -0,0 +1,11 @@
+/*
+input: 'abc'
+output: ['abc', 'acb', 'bac', 'bca', 'cab', 'cba]
+*/
+
+// medium problem
+
+const permutationsOfString = (str) => {};
+
+// need to swap positions of input
+const helper = (input, index = i + 1) => {};
