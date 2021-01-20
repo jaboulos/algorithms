da4555d0bf20edc4570df8318eab1a1bbad13b81
jaboulos <jab12288@gmail.com>
2021-01-20 15:37:42 -0800

    Remove src layer

---
+++ trees/singleValueTree/singleValueTree.js
@@ -0,0 +1,16 @@
+/*
+Single Value Tree
+
+Given a binary tree, find the number of unival subtrees.
+An unival tree is a tree that has the same value in every node.
+
+Notes
+Input Parameters: Function has one argument: root of the tree.
+Output: Function must return an integer: the number of the unival subtrees in the given tree.
+
+
+Constraints:
+0 <= number of nodes in the tree <= 10^5
+-10^9 <= value of any node <= 10^9
+
+*/
