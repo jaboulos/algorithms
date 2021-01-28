f3130417bf8f135eed1171414ef27381039c72ba
jaboulos <jab12288@gmail.com>
2021-01-28 14:22:59 -0800

    Add algorithm and solution

---
+++ graphs/courseSchedule/courseSchedule3.js
@@ -0,0 +1,78 @@
+/*
+There are a total of numCourses courses you have to take, labeled from 0 to numCourses-1.
+Some courses may have prerequisites, for example to take course 0 you have to first take course 1, which is expressed as a pair: [0,1]
+Given the total number of courses and a list of prerequisite pairs, is it possible for you to finish all courses?
+
+Example 1:
+Input: numCourses = 2, prerequisites = [[1,0]]
+Output: true
+Explanation: There are a total of 2 courses to take.
+             To take course 1 you should have finished course 0. So it is possible.
+
+Example 2:
+Input: numCourses = 2, prerequisites = [[1,0],[0,1]]
+Output: false
+Explanation: There are a total of 2 courses to take.
+             To take course 1 you should have finished course 0, and to take course 0 you should
+             also have finished course 1. So it is impossible.
+
+
+Constraints:
+The input prerequisites is a graph represented by a list of edges, not adjacency matrices. Read more about how a graph is represented.
+You may assume that there are no duplicate edges in the input prerequisites.
+1 <= numCourses <= 10^5
+*/
+
+var canFinish = function (numCourses, prerequisites) {
+  graph = new Map();
+  visiting = new Set();
+  visited = new Set();
+
+  // this populates the graph instantiated on line 7
+  for (let [v, e] of prerequisites) {
+    if (graph.has(v)) {
+      let edges = graph.get(v);
+      edges.push(e);
+      graph.set(v, edges);
+    } else {
+      graph.set(v, [e]);
+    }
+  }
+
+  for (const [v, e] of graph) {
+    if (DFS(v)) {
+      return false; //if cyclic it will not finish so it is false
+    }
+  }
+
+  return true;
+};
+
+var DFS = function (v) {
+  visiting.add(v);
+  let edges = graph.get(v); // get all the edges to explore
+
+  if (edges) {
+    //console.log(edges)
+    for (let e of edges) {
+      if (visited.has(e)) {
+        //skip if it is explored already
+        continue;
+      }
+
+      if (visiting.has(e)) {
+        //found e is being explored
+        return true;
+      }
+
+      if (DFS(e)) {
+        // DFS deeper if this e is cyclic
+        return true;
+      }
+    }
+  }
+
+  visiting.delete(v); // remove from visiting set when all decedant v are visited
+  visited.add(v);
+  return false;
+};
