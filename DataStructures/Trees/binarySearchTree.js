4a09c285e1013df9a727f34edaf8efe9cea1401b
jaboulos <jab12288@gmail.com>
2021-02-23 15:58:36 -0800

    Add bst insert and find methods

--- DataStructures/Trees/binarySearchTree.js
+++ DataStructures/Trees/binarySearchTree.js
@@ -9,3 +9,62 @@ BST:
 - Sorted in a particular order (inorder, preorder, postorder)
 
 */
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+// starts with empty root
+class BST {
+  constructor() {
+    this.root = null;
+  }
+
+  // insert node method
+  insert(value) {
+    let newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    }
+    let current = this.root;
+    while (true) {
+      if (value === current.value) return undefined;
+      if (value < current.value) {
+        if (current.left === null) {
+          current.left = newNode;
+          return this;
+        }
+        current = current.left;
+      } else if (value > current.value) {
+        current.right = newNode;
+        return this;
+      }
+      current = current.right;
+    }
+  }
+  // find value method
+  find(value) {
+    if (this.root === null) return false;
+    let current = this.root;
+    let found = false;
+
+    while (current && !found) {
+      if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      } else {
+        found = true;
+      }
+    }
+    return current || false;
+  }
+}
+
+// insert is O(logn)
+// search is O(logn)
