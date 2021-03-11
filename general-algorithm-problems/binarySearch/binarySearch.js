b1eac1ac88fdd0df8588ddf6a3473b794e612be3
jaboulos <jab12288@gmail.com>
2021-03-11 15:18:59 -0800

    Cleanup binarySearch solution

--- general-algorithm-problems/binarySearch/binarySearch.js
+++ general-algorithm-problems/binarySearch/binarySearch.js
@@ -1,40 +1,19 @@
-// const binarySearch = (arr, target) => {
-//   if(arr.length < 1) return -1;
-
-//   let left = 0;
-//   let right = arr.length -1
-//   let mid = Math.floor((arr.length-1)/2)
-
-//   if(arr[mid] === target) return mid
-
-//   while(left < right) {
-//     if(arr[left] === target) {
-//       return left
-//     } else if(arr[right] === target) {
-//       return right
-//     } else {
-//       left++
-//       right--
-//     }
-//   }
-//   return -1
-// }
-
-// binarySearch([1,2,3,4,5], 2) // 1
-// binarySearch([1,2,3,4,5], 3) // 2
-// binarySearch([1,2,3,4,5], 5) // 4
-// binarySearch([1,2,3,4,5], 100) // -1
+// using a binary search approach, write a function that accepts an arr and target
+// return index of the target, if target is not present return -1
 
 const binarySearch = (arr, target) => {
   if (arr.length < 1) return -1;
 
+  // define pointers
   let left = 0;
   let right = arr.length - 1;
   let mid = Math.floor((arr.length - 1) / 2);
 
+  // check if mid is the target value
   if (arr[mid] === target) return mid;
 
   while (arr[mid] !== target && left <= right) {
+    // divide and conquer
     if (target < arr[mid]) {
       right = mid - 1;
     } else {
